af98b84b23820e730c21937817ae16778fe654ab
Adrian S <adik993@gmail.com>
2017-04-08 15:42:26 +0200

    Removed unused npm dependency

--- gui/src/store/configureStore.js
+++ gui/src/store/configureStore.js
@@ -3,18 +3,6 @@ import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "reducers";
 import {createReduxStomp} from "redux-stomp";
-// import { startReduxClient, actionCreator, reduxSockjs, createReducer } from 'redux-sockjs'
-// import SockJS from "sockjs-client";
-// import Stomp from "stompjs";
-//
-// const socket = SockJS('/gs-guide-websocket');
-// const stomp = Stomp.over(socket);
-// stomp.connect({}, frame => {
-//     console.log(frame);
-//     stomp.subscribe('/client/providers', message => {
-//         console.log(message);
-//     });
-// });
 
 console.log(process.env.NODE_ENV);
 
