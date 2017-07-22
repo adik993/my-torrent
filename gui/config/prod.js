6221e1d669338d60234fd230cb4d841d9e6dd4d5
Adrian Skrobacz <adik993@gmail.com>
2017-07-22 19:56:50 +0200

    Complete gradle setup

--- gui/config/prod.js
+++ gui/config/prod.js
@@ -1,13 +1,13 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const path = require('path');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const options = require('./base');
 
-module.exports = function (env) {
+module.exports = function () {
     return webpackMerge(options(), {
         output: {
-            path: path.join(__dirname, '../target', 'classes', 'public'),
+            path: path.join(__dirname, '../build', 'classes', 'public'),
             sourceMapFilename: "[name].map"
         },
         plugins: [
