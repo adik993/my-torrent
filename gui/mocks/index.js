3c5f83b9569d9650ff6dc2d5efea1e38f9beb68b
Adrian Skrobacz <adik993@gmail.com>
2017-07-23 21:50:45 +0200

    Extend mocks to support search and providers

--- gui/mocks/index.js
+++ gui/mocks/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const providersJson = require('./json/providers.json');
+const searchFlashS01E01Json = require('./json/search-flash-s01e01.json');
+const searchFlashS01E02Json = require('./json/search-flash-s01e02.json');
 
 app.get('/api/user', function (req, resp) {
     resp.send(JSON.stringify({name: 'John', surname: 'Doe'}));
 });
 
-app.listen(5000, function () {
-    console.log('Example app listening on port 5000!');
+app.get('/api/providers', function (req, resp) {
+    resp.send(providersJson);
+});
+
+app.get('/api/search', function (req, resp) {
+    if ('Flash S01E01' === req.query.query) {
+        resp.send(searchFlashS01E01Json)
+    } else if ('Flash S01E02' === req.query.query) {
+        resp.send(searchFlashS01E02Json)
+    } else {
+        resp.send([])
+    }
+});
+
+const PORT = 8080;
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
 });
