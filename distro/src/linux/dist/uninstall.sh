05cdf5f6407db6eac4eae14d9cbaf25875cb2739
Adrian Skrobacz <adik993@gmail.com>
2017-10-26 22:31:28 +0200

    Revert deleted distributions' files

---
+++ distro/src/linux/dist/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+install_dir=`pwd`
+
+sudo systemctl stop my-torrent.service
+sudo systemctl disable my-torrent.service
+
+cd ..
+echo "Removing $install_dir"
+sudo rm -r $install_dir
+
+sudo userdel mytorrent
